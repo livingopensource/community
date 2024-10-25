a2747e30e57073168a8bf5e100b2c268dbba34b5
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-10-25 16:19:22 +0200

    feat: Added web site pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+	//import logo from '$lib/assets/images/LOSF Orange.png?enhanced';
+	import logoImg from '$lib/assets/images/LOSF Orange.png';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Footer, FooterCopyright, FooterLinkGroup, FooterBrand, FooterLink } from 'flowbite-svelte';
+</script>
+
+<Navbar class="fixed z-10 shadow-2xl">
+  <NavBrand href="/">
+    <img src={logoImg} class="me-3 h-6 sm:h-9" alt="Convo Logo" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Conferences</span>
+  </NavBrand>
+  <NavHamburger  />
+  <NavUl >
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
+
+{@render children()}
+
+<Footer footerType="logo">
+	<div class="sm:flex sm:items-center sm:justify-between">
+	<FooterBrand href="https://livingopensource.org" src={logoImg} alt="Logo" name="LOSF" />
+	  <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+		<FooterLink href="/">About</FooterLink>
+		<FooterLink href="/">Privacy Policy</FooterLink>
+		<FooterLink href="/">Licensing</FooterLink>
+		<FooterLink href="/">Contact</FooterLink>
+	  </FooterLinkGroup>
+	</div>
+	<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+	<FooterCopyright href="/" by="Living Open Source Foundation" year={new Date().getFullYear()} />
+</Footer>
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+import flowbitePlugin from 'flowbite/plugin'
+
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {"50":"#fff7ed","100":"#ffedd5","200":"#fed7aa","300":"#fdba74","400":"#fb923c","500":"#f97316","600":"#ea580c","700":"#c2410c","800":"#9a3412","900":"#7c2d12"}
+			  }
+		}
+	},
+
+	plugins: [flowbitePlugin]
+} as Config;
