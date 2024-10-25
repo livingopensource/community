20f768ae5f23bc675febc03e2531f15494168ba0
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-10-25 16:31:41 +0200

    feat: Added visible main page content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	import { Footer, FooterCopyright, FooterLinkGroup, FooterBrand, FooterLink } from 'flowbite-svelte';
 </script>
 
-<Navbar class="fixed z-10 shadow-2xl">
+<Navbar class="shadow-2xl">
   <NavBrand href="/">
     <img src={logoImg} class="me-3 h-6 sm:h-9" alt="Convo Logo" />
     <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Conferences</span>
@@ -23,14 +23,14 @@
 
 {@render children()}
 
-<Footer footerType="logo">
+<Footer footerType="logo" class="">
 	<div class="sm:flex sm:items-center sm:justify-between">
-	<FooterBrand href="https://livingopensource.org" src={logoImg} alt="Logo" name="LOSF" />
+	<FooterBrand href="https://livingopensource.org" src={logoImg} alt="Logo" name="Living Open Source Foundation" />
 	  <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-		<FooterLink href="/">About</FooterLink>
-		<FooterLink href="/">Privacy Policy</FooterLink>
-		<FooterLink href="/">Licensing</FooterLink>
-		<FooterLink href="/">Contact</FooterLink>
+		<FooterLink href="/about">About</FooterLink>
+		<FooterLink href="/pricing">Privacy Policy</FooterLink>
+		<FooterLink href="/license">Licensing</FooterLink>
+		<FooterLink href="/contact">Contact</FooterLink>
 	  </FooterLinkGroup>
 	</div>
 	<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,7 @@
 
 </script>
 
+<div class="container">
+    <h1>Welcome</h1>
+</div>
 
