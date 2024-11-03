d1c75e40250b17cecfe7126509672f66b41a1361
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-11-03 12:45:21 +0200

    fix: Time line data alignment

--- src/routes/(pages)/+page.svelte
+++ src/routes/(pages)/+page.svelte
@@ -32,9 +32,9 @@
       </div>
       
       <div class="flex justify-evenly">
-        <Timeline order="horizontal" class="md:flex content-evently">
+        <!-- <Timeline order="horizontal" class="md:flex content-evently">
           <TimelineItem title="Stimulate Local Economies" date="01">
-            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">By developing local Open Source expertise, we enable communities to save money on software and invest in other critical areas like infrastructure, education, and healthcare. This helps to stimulate economic growth and create sustainable development opportunities.</p>
+            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Our task is to develop and foster growth of the local digital ecosystem through the use of open source software..</p>
           </TimelineItem>
           
           <TimelineItem title="Promoting Inclusive Software" date="02">
@@ -52,7 +52,102 @@
           <TimelineItem title="Open Source Consultation" date="06">
             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Our local knowledge centers serve as one-stop shops for all things Open Source. They offer consulting services to local enterprises, governments, and non-profit organizations, helping them transition to and benefit from Open Source solutions.</p>
           </TimelineItem>
-        </Timeline>
+        </Timeline> -->
+
+<ol class="items-center md:flex">
+  <li class="relative mb-6 sm:mb-0">
+      <div class="flex items-center">
+          <div class="z-10 flex items-center justify-center w-6 h-6 bg-orange-100 rounded-full ring-0 ring-white dark:bg-orange-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+              <svg class="w-2.5 h-2.5 text-orange-800 dark:text-orange-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+              </svg>
+          </div>
+          <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+      </div>
+      <div class="mt-3 sm:pe-8">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Stimulate Local Economies</h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">01</time>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">Our task is to develop and foster growth of the local digital ecosystem through the use of open source software..</p>
+      </div>
+  </li>
+  <li class="relative mb-6 sm:mb-0">
+      <div class="flex items-center">
+          <div class="z-10 flex items-center justify-center w-6 h-6 bg-orange-100 rounded-full ring-0 ring-white dark:bg-orange-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+              <svg class="w-2.5 h-2.5 text-orange-800 dark:text-orange-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+              </svg>
+          </div>
+          <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+      </div>
+      <div class="mt-3 sm:pe-8">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Promoting Inclusive Software</h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">02</time>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">Open Source software does not discriminate and provides equal opportunities for everyone. It allows individuals from all backgrounds to contribute, learn, and grow, thereby promoting inclusivity and diversity in the tech community.</p>
+      </div>
+  </li>
+  <li class="relative mb-6 sm:mb-0">
+      <div class="flex items-center">
+          <div class="z-10 flex items-center justify-center w-6 h-6 bg-orange-100 rounded-full ring-0 ring-white dark:bg-orange-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+              <svg class="w-2.5 h-2.5 text-orange-800 dark:text-orange-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+              </svg>
+          </div>
+          <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+      </div>
+      <div class="mt-3 sm:pe-8">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Guided Training Process</h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">03</time>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">We follow a structured three-step approach to training. Starting with basic education at large events, we move on to more advanced certification for experienced participants, and finally, we establish local knowledge centers to provide ongoing, affordable training.</p>
+      </div>
+  </li>
+  <li class="relative mb-6 sm:mb-0">
+    <div class="flex items-center">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-orange-100 rounded-full ring-0 ring-white dark:bg-orange-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+            <svg class="w-2.5 h-2.5 text-orange-800 dark:text-orange-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </div>
+        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+    </div>
+    <div class="mt-3 sm:pe-8">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Community Building</h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">04</time>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">We establish local communities through on-site events where we teach the basics of Open Source software. These events help create a network of local enthusiasts and professionals who continue to meet and grow together, ensuring sustained development and support.</p>
+    </div>
+  </li>
+  <li class="relative mb-6 sm:mb-0">
+    <div class="flex items-center">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-orange-100 rounded-full ring-0 ring-white dark:bg-orange-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+            <svg class="w-2.5 h-2.5 text-orange-800 dark:text-orange-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </div>
+        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+    </div>
+    <div class="mt-3 sm:pe-8">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">International Collaboration</h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">05</time>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">We partner with international organizations like ITGilde to bring global expertise to local communities. This collaboration enhances our training programs and ensures that local talent can participate in the worldwide Open Source ecosystem on an equal footing.</p>
+    </div>
+  </li>
+  <li class="relative mb-6 sm:mb-0">
+    <div class="flex items-center">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-orange-100 rounded-full ring-0 ring-white dark:bg-orange-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+            <svg class="w-2.5 h-2.5 text-orange-800 dark:text-orange-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </div>
+        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+    </div>
+    <div class="mt-3 sm:pe-8">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Open Source Consultation</h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">06</time>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Our local knowledge centers serve as one-stop shops for all things Open Source. They offer consulting services to local enterprises, governments, and non-profit organizations, helping them transition to and benefit from Open Source solutions.</p>
+    </div>
+  </li>
+</ol>
+
+
       </div>
     </div>
   </div>
