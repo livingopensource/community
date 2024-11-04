8b67d4fa12a9922d6257d1a5915c6999edb43c88
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-11-04 17:38:42 +0200

    feat: Added Custom data editing for membership items

--- src/routes/(dashboard)/dash/membership/[type]/+page.svelte
+++ src/routes/(dashboard)/dash/membership/[type]/+page.svelte
@@ -1,23 +1,72 @@
 <script lang="ts">
   import {
-    Card
+    Card,
+    Badge
   } from 'flowbite-svelte';
+  import {
+    EditOutline
+  } from 'flowbite-svelte-icons';
+  import { onMount } from 'svelte'; 
   import type { PageServerData } from './$types'; 
   let {data}: {data: PageServerData} = $props();
   import Editor from '@tinymce/tinymce-svelte';
   let conf = {
+    skin: 'oxide-dark',
     height: 500,
     menubar: false,
     plugins: [
       'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
       'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-      'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+      'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount',
+      'emoticons', 'table'
     ],
     toolbar: 'undo redo | blocks | ' +
-      'bold italic forecolor | alignleft aligncenter ' +
+      'bold italic forecolor | table alignleft aligncenter ' +
       'alignright alignjustify | bullist numlist outdent indent | ' +
-      'removeformat | help',
+      'removeformat | emoticons | fullscreen'
   }
+  let membershipData: {
+    editingObject: string,
+    name: string,
+    title: string,
+    description: string,
+    amount: number
+  } = $state({
+    editingObject: 'secription',
+    name: data.membership.name,
+    title: data.membership.subTitle,
+    description: data.membership.description,
+    amount: data.membership.amount,
+  });
+  let content = $state(data.membership.content);
+  onMount(() => {
+    membershipData = {
+    editingObject: 'secription',
+    name: data.membership.name,
+    title: data.membership.subTitle,
+    description: data.membership.description,
+    amount: data.membership.amount,
+  }
+  })
+
+  function debounceEdit(data: string) {
+   switch (membershipData.editingObject) {
+     case 'name':
+       membershipData.name = data;
+       console.log(data+" "+membershipData.name+" "+membershipData.editingObject);
+       return membershipData
+     case 'title':
+       membershipData.title = data;
+       return membershipData
+     case 'description':
+       membershipData.description = data;
+       return membershipData
+     default:
+       return membershipData;
+   }
+  }
+
+  // membershipData = $derived(debounceEdit(content))
 </script>
   <svelte:head>
     <title> LOSF Conference | Dashboard Membership</title>
@@ -33,13 +82,45 @@
           <h1 class="dark:text-white">Membership Subscriptions</h1>
           <br />
           <Editor
+            cssClass="material-classic"
             licenseKey='gpl'
             scriptSrc='/tinymce/tinymce.min.js'
-            value={data.membership.description}
+            value={content}
             {conf}
           />
         </div>
         <div class="flex-auto w-14">
+          <Card class="text-left">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {membershipData.name}
+                  <EditOutline onclick={()=> {
+                    content = membershipData.name;
+                    membershipData.editingObject = 'name';
+                  }}/>
+              </h5>
+              
+              <p class="mb-5 p-5 font-xs text-gray-700 dark:text-gray-400">
+                  {membershipData.title}
+                  <EditOutline onclick={()=> {
+                    content = membershipData.title
+                    membershipData.editingObject = 'title';
+                  }}/>
+              </p>
+              
+              <Badge rounded large color="dark" class="text-2xl font-bold">{membershipData.amount}</Badge>
+              <EditOutline onclick={()=> {
+                content = membershipData.amount
+                membershipData.editingObject = 'amount';
+              }}/>
+              <p class="mt-5 font-normal text-gray-700 dark:text-gray-400">
+                  {membershipData.description}
+                  <EditOutline onclick={()=> {
+                    content = membershipData.description
+                    membershipData.editingObject = 'description';
+                  }}/>
+              </p>
+              <br />
+          </Card>
           <h1 class="dark:text-white">
             You don't have an active membership subscription
           </h1>
