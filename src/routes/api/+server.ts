7891d9e2bed55a773ad25eb58c9cb38a012bdd31
Arthur Kalikiti <arthurkalikiti@gmail.com>
2025-03-28 14:39:57 +0200

    refactor: Removed sequelize from the pacjage.json and other files that depend on it

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,17 +1,1 @@
-import { DBConn } from "$lib/server/databases/pg/init";
-import { Membership, Subscription } from "$lib/server/databases/pg/memberships";
-import { Permission, Role } from "$lib/server/databases/pg/permissions";
-import { Session, User } from "$lib/server/databases/pg/users";
-import { json } from "@sveltejs/kit";
-
-export async function GET() {
-    const sequelize = DBConn
-    await sequelize.sync({alter: true})
-    User.findAll()
-    Session.findAll()
-    Membership.findAll()
-    Subscription.findAll()
-    Permission.findAll()
-    Role.findAll()
-    return json({"message": "hello world"})
-}
+export async function GET() {}
